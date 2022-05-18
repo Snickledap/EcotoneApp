e9e766730dbbc447a8b709febb5d14aecb5fb9a0
fenghun31 <fujiahao31@gmail.com>
2022-05-18 14:16:38 -0400

    test

---
+++ routes/routes/nested-bottom-navigation-bar/bottom_navigation.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:ecotone_app/routes/nested-bottom-navigation-bar/tab_item.dart';
+
+class BottomNavigation extends StatelessWidget {
+  BottomNavigation({required this.currentTab, required this.onSelectTab});
+  final TabItem currentTab;
+  final ValueChanged<TabItem> onSelectTab;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: [
+        _buildItem(TabItem.amber),
+        _buildItem(TabItem.green),
+        _buildItem(TabItem.blue),
+      ],
+      onTap: (index) => onSelectTab(
+        TabItem.values[index],
+      ),
+      currentIndex: currentTab.index,
+      selectedItemColor: activeTabColor[currentTab]!,
+    );
+  }
+
+  BottomNavigationBarItem _buildItem(TabItem tabItem) {
+    return BottomNavigationBarItem(
+      icon: Icon(
+        Icons.layers,
+        color: _colorTabMatching(tabItem),
+      ),
+      label: tabName[tabItem],
+    );
+  }
+
+  Color _colorTabMatching(TabItem item) {
+    return currentTab == item ? activeTabColor[item]! : Colors.grey;
+  }
+}
