af5b5be3065472534bd89bdf6f9b8c3cdf718ff5
fenghun31 <fujiahao31@gmail.com>
2022-05-19 13:39:47 -0400

    got rid of the list of colors

--- lib/routes/nested-bottom-navigation-bar/colors_list_page.dart
+++ lib/routes/nested-bottom-navigation-bar/colors_list_page.dart
@@ -17,36 +17,8 @@ class ColorsListPage extends StatelessWidget {
         ),
         body: Container(
           color: Colors.white,
-          child: _buildList(),
-        ));
-  }
 
-  final List<int> materialIndices = [
-    900,
-    800,
-    700,
-    600,
-    500,
-    400,
-    300,
-    200,
-    100,
-    50
-  ];
-
-  Widget _buildList() {
-    return ListView.builder(
-        itemCount: materialIndices.length,
-        itemBuilder: (BuildContext content, int index) {
-          int materialIndex = materialIndices[index];
-          return Container(
-            color: color[materialIndex],
-            child: ListTile(
-              title: Text('$materialIndex', style: TextStyle(fontSize: 24.0)),
-              trailing: Icon(Icons.chevron_right),
-              onTap: () => onPush?.call(materialIndex),
-            ),
-          );
-        });
+        )
+    );
   }
 }
