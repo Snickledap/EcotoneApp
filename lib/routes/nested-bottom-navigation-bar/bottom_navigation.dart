f3caf1b7ecdf221859f5728a1eb2ff820b2a0233
fenghun31 <fujiahao31@gmail.com>
2022-05-19 13:14:58 -0400

    changed bottom navigation bar to color black

--- lib/routes/nested-bottom-navigation-bar/bottom_navigation.dart
+++ lib/routes/nested-bottom-navigation-bar/bottom_navigation.dart
@@ -3,38 +3,41 @@ import 'package:ecotone_app/routes/nested-bottom-navigation-bar/tab_item.dart';
 
 class BottomNavigation extends StatelessWidget {
   BottomNavigation({required this.currentTab, required this.onSelectTab});
+
   final TabItem currentTab;
   final ValueChanged<TabItem> onSelectTab;
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      items: [
-        _buildItem(TabItem.map),
-        _buildItem(TabItem.checklist),
-        _buildItem(TabItem.help),
-        _buildItem(TabItem.profile)
-      ],
-      onTap: (index) => onSelectTab(
-        TabItem.values[index],
-      ),
-      currentIndex: currentTab.index,
-      selectedItemColor: activeTabColor[currentTab]!,
+        backgroundColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          _buildItem(TabItem.map),
+          _buildItem(TabItem.checklist),
+          _buildItem(TabItem.help),
+          _buildItem(TabItem.profile)
+        ],
+        onTap: (index) =>
+            onSelectTab(
+              TabItem.values[index],
+            ),
+        currentIndex: currentTab.index,
+        selectedItemColor: activeTabColor[currentTab]!,
+        selectedLabelStyle: TextStyle(color: Colors.orange)
     );
   }
 
   BottomNavigationBarItem _buildItem(TabItem tabItem) {
     return BottomNavigationBarItem(
       icon: Icon(tabIcon[tabItem],
-      color: _colorTabMatching(tabItem),
+        color: _colorTabMatching(tabItem),
       ),
       label: tabName[tabItem],
-      );
-
+    );
   }
 
   Color _colorTabMatching(TabItem item) {
-    return currentTab == item ? activeTabColor[item]! : Colors.grey;
+    return currentTab == item ? activeTabColor[item]! : Colors.white;
   }
 }
