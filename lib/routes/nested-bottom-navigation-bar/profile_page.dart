084a774a51e0ab81d48f6fad7aea5329d7919ef7
fenghun31 <fujiahao31@gmail.com>
2022-05-22 19:47:26 -0400

    working in progress:
    separated profile_page.dart from all other dart files for test runs
    
    added buttons and reminders to profile_page.dart
    added package flutter_custom_cards.dart for custom cards in order to add reminders
    added package sizer to auto adjust screen aspect ratio
    
    issue: when ran, LateinitializationError occurs

--- lib/routes/nested-bottom-navigation-bar/profile_page.dart
+++ lib/routes/nested-bottom-navigation-bar/profile_page.dart
@@ -1,25 +1,106 @@
+import 'package:ecotone_app/main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_cards/flutter_custom_cards.dart';
+import 'package:sizer/sizer.dart';
 
-class profilePage extends StatefulWidget{
+
+
+void main() => runApp(ProfilePage());
+
+class ProfilePage extends StatelessWidget {
+
   @override
-  _ProfileState createState() => _ProfileState();
-}
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar:
+          AppBar(
+            title: const Text(
+                'Profile',
+                ),
+            titleTextStyle:
+              const TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+              ),
+            centerTitle: true,
+            backgroundColor: const Color(0xFF309BE9), //Ecotone Colors
+          ),
+        body:
+            Column(
+              children:<Widget>[
+                  Container(
+                    height: 10.0.h,
+                        width: 80.0.w,
+                        child: Column(
+                          children: <Widget>[
+                            ListView(
+                              scrollDirection: Axis.horizontal,
+                              children:  <Widget>[
+                                CustomCard(
+                                  borderRadius:10,
+                                  color: Color(0x10FFFFF),
+                                  child: ListTile(
+                                    title: Text('time of the event'),
+                                    subtitle: Text('name of the event'),
+                                    trailing:
+                                    Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: <Widget>[
+                                        IconButton(
+                                          icon: Icon(
+                                            Icons.alarm_add_outlined,
+                                            size: 20.0,
+                                            color: Colors.brown[900],
+                                          ),
+                                          onPressed: () {},
+                                        ),
+                                        IconButton(
+                                          icon: Icon(
+                                            Icons.delete_outline,
+                                            size: 20.0,
+                                            color: Colors.brown[900],
+                                          ),
+                                          onPressed: () {},
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                     ),
 
-class _ProfileState extends State<profilePage>{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.orange,
-        title: Text('Profile') ,
-        centerTitle: true,
-      ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
+             Padding(padding: EdgeInsets.all(10)),
+             Align(alignment: Alignment.center),
+              ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                onPrimary: Colors.white,
+                elevation: 4,
+              ),
+              onPressed: (){},
+                child: const Text('Sign Out')
+              ),
+              const Padding(padding: EdgeInsets.all(20)),
+              ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                onPrimary: Colors.white,
+                elevation: 4,
+              ),
+              onPressed: (){},
+                child: const Text('Involvement')
+            ),
 
-        )
+   ],
+            ),
       ),
-      );
+    );
+
+
   }
-}
+  }
+
