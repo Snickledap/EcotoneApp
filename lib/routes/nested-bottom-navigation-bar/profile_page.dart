c7bc97ae7091c7ed1d84f2138c16b612f0be2b9c
fenghun31 <fujiahao31@gmail.com>
2022-05-23 13:28:55 -0400

    working in progress: [FIXED]issue: when ran, LateinitializationError occurs
    custom cards for reminder
    vertically scrolling.
    need to be changed to two rows of horizontally scrolling cards. (based on the figma)
    need to add:
    bottom navigation bar
    page transition for sign out button and involvement button

--- lib/routes/nested-bottom-navigation-bar/profile_page.dart
+++ lib/routes/nested-bottom-navigation-bar/profile_page.dart
@@ -14,93 +14,110 @@ class ProfilePage extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar:
-          AppBar(
-            title: const Text(
-                'Profile',
-                ),
-            titleTextStyle:
-              const TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-              ),
-            centerTitle: true,
-            backgroundColor: const Color(0xFF309BE9), //Ecotone Colors
+        AppBar(
+          title: const Text(
+            'Profile',
           ),
+          titleTextStyle:
+          const TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+          centerTitle: true,
+          backgroundColor: const Color(0xFF309BE9), //Ecotone Colors
+        ),
         body:
-            Column(
-              children:<Widget>[
-                  Container(
-                    height: 10.0.h,
-                        width: 80.0.w,
-                        child: Column(
-                          children: <Widget>[
-                            ListView(
-                              scrollDirection: Axis.horizontal,
-                              children:  <Widget>[
-                                CustomCard(
-                                  borderRadius:10,
-                                  color: Color(0x10FFFFF),
-                                  child: ListTile(
-                                    title: Text('time of the event'),
-                                    subtitle: Text('name of the event'),
-                                    trailing:
-                                    Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: <Widget>[
-                                        IconButton(
-                                          icon: Icon(
-                                            Icons.alarm_add_outlined,
-                                            size: 20.0,
-                                            color: Colors.brown[900],
-                                          ),
-                                          onPressed: () {},
-                                        ),
-                                        IconButton(
-                                          icon: Icon(
-                                            Icons.delete_outline,
-                                            size: 20.0,
-                                            color: Colors.brown[900],
-                                          ),
-                                          onPressed: () {},
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                     ),
-
-             Padding(padding: EdgeInsets.all(10)),
-             Align(alignment: Alignment.center),
-              ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red,
-                onPrimary: Colors.white,
-                elevation: 4,
+        Container(
+          alignment: Alignment.bottomCenter,
+          margin: EdgeInsets.all(25),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 500,
+                width: 500,
+                child: _CardList(),
               ),
-              onPressed: (){},
+            Padding(padding: EdgeInsets.all(10)),
+            Align(alignment: Alignment.bottomCenter),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                  onPrimary: Colors.white,
+                  elevation: 4,
+                ),
+                onPressed: () {},
                 child: const Text('Sign Out')
-              ),
-              const Padding(padding: EdgeInsets.all(20)),
-              ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                primary: Colors.red,
-                onPrimary: Colors.white,
-                elevation: 4,
-              ),
-              onPressed: (){},
+            ),
+            const Padding(padding: EdgeInsets.all(20)),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                  onPrimary: Colors.white,
+                  elevation: 4,
+                ),
+                onPressed: () {},
                 child: const Text('Involvement')
             ),
+          ],
+        )
+      )
+    )
+   );
+}
+}
 
-   ],
+
+  @override
+  class _customcards extends StatelessWidget{
+  @override
+    Widget build (BuildContext context) {
+          return CustomCard(
+          borderRadius: 15,
+          color: Color(0x10FFFFF),
+          child: ListTile(
+            title: Text('time of the event'),
+            subtitle: Text('name of the event'),
+            trailing:
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+            IconButton(
+            icon: Icon(
+              Icons.alarm_add_outlined,
+              size: 20.0,
+              color: Colors.brown[900],
             ),
-      ),
+            onPressed: () {},
+          ),
+        ],
+            ),
+          ),
+        );
+    }
+  }
+
+class _CardList extends StatelessWidget{
+  @override
+  Widget build (BuildContext context){
+    return ListView.builder(
+        shrinkWrap: true,
+        itemCount: 5,
+        scrollDirection: Axis.vertical,
+        itemBuilder:(context,index){
+      return Container(
+        height: 100,
+        width: 100,
+        child: ListView(
+        children: <Widget>[
+         _customcards()
+        ],
+        ),
+      );
+  }
     );
+  }
+
+}
 
 
-  }
-  }
 
