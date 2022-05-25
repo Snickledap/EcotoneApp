afb64f899e0c3c867d3adecec726de70ff43d455
fenghun31 <fujiahao31@gmail.com>
2022-05-25 14:36:01 -0400

    extra text fields
    [WIP]
    focus on only one textfield when clicked on
    
    adding a title to each text field
    
    making the entire page stateful instead of stateless
    
    scrollview for the entire form

--- lib/routes/SeaHorse_Data_Input_Form.dart
+++ lib/routes/SeaHorse_Data_Input_Form.dart
@@ -1,24 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:ecotone_app/NavBar.dart';
 
 
-void main() => runApp(BioGasForm());
-class BioGasForm extends StatelessWidget {
+void main() => runApp(SeaHorseDataForm());
+class SeaHorseDataForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Bio Gas Form'),
+          centerTitle: true,
+          title: Text('Sea Horse Data Input Form'),
         ),
         body:Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
            _TextBoxes(),
+            _TextBoxes(),
+            _TextBoxes(),
+            _TextBoxes(),
+            _TextBoxes(),
+            _TextBoxes(),
             _TextBoxes(),
             _SubmitButton(),
           ],
         ),
+        bottomNavigationBar: NavBar(),
       ),
 
     );
@@ -30,11 +38,13 @@ class _TextBoxes extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextField(
+
       style: TextStyle(
         color: Colors.black,
         fontFamily: 'OpenSans',
       ),
       decoration: InputDecoration(
+        label: Text('Name'),
         border: OutlineInputBorder(),
         hintText: 'Your Answer',
         contentPadding: EdgeInsets.all( 10.0),
