bbb29824e99cdcaf9f05ef180eee7b0bd695e876
fenghun31 <fujiahao31@gmail.com>
2022-06-06 12:31:36 -0400

    google maps is showing
    [WIP] adding the positions of the containers

--- lib/routes/map/Map_Page.dart
+++ lib/routes/map/Map_Page.dart
@@ -1,79 +1,40 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+void main() => runApp(const MyApp());
 
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
 
-void main() {
-  runApp(Map());
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
+class _MyAppState extends State<MyApp> {
+  late GoogleMapController mapController;
 
-class Map extends StatelessWidget{
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home:Scaffold(
+      home: Scaffold(
         appBar: AppBar(
-          centerTitle: true,
-          title: Text('Map'),
+          title: const Text('Maps Sample App'),
+          backgroundColor: Colors.green[700],
         ),
-          body: Column(
-          children: <Widget>[
-           Container(
-             height: 400,
-             width: 500,
-             child: googleMap(),
-           )
-            //map
-            //list
-          ],
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
         ),
-      )
-    );
-  }
-}
-
-
-class googleMap extends StatefulWidget{
-
-  @override
-  State<googleMap> createState() => googleMapState();
-}
-
-class googleMapState extends State<googleMap>{
-  final Completer<GoogleMapController> _controller = Completer();
-
-  static final CameraPosition _kBrottier = CameraPosition(
-    target: LatLng(40.437509, -79.993047),
-    zoom: 14.4746,
-  );
-
-  static final CameraPosition _kHome = CameraPosition(
-      target: LatLng(40.64221310404301, -79.93630650628933),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kBrottier,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goHome,
-        label: Text('Go Home'),
-        icon: Icon(Icons.house),
       ),
     );
-  }
-
-  Future<void> _goHome() async {
-    final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kHome));
   }
 }
