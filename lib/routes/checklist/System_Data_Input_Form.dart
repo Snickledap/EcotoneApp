9312a81a552971f8a7eac9599294705acb45a553
fenghun31 <fujiahao31@gmail.com>
2022-06-10 10:48:04 -0400

    re-organized the layout of the code,
    added sizer to the page

--- lib/routes/checklist/System_Data_Input_Form.dart
+++ lib/routes/checklist/System_Data_Input_Form.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:ecotone_app/NavBar.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sizer/sizer.dart';
 
 
 
@@ -18,8 +19,12 @@ class SystemInputFormPage extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
-        home:DataForm()
+    return Sizer(builder: (context, orientation, deviceType)
+    {
+      return MaterialApp(
+          home: DataForm()
+      );
+    }
     );
   }
 }
@@ -85,214 +90,204 @@ class FormListState extends State<FormList>{
   @override
   Widget build (BuildContext context){
     CollectionReference SystemDataInputForm = FirebaseFirestore.instance.collection('System_Data_Input_Form');
-    return SingleChildScrollView(
-      physics: NeverScrollableScrollPhysics(),
-      reverse: true,
-      child: Column(
-        children:<Widget>[
-         Name, // replace after back end is connected for further customization
-         SizedBox(
-           height:350,
-           child: InkWell(
-             splashColor: Colors.transparent,
-             child: SingleChildScrollView(
-               padding: const EdgeInsets.only(
-                 top: 5,
-                 bottom: 5,
-                 left: 5,
-                 right: 5,
-               ),
-              reverse: true,
-               child: Form(
-                key: _formKey,
-                  child: Column(
-                  children: <Widget>[
-                    FormField(
-                      builder: (FormFieldState<String> state) {
-                        return InputDecorator(
-                          decoration: InputDecoration(
-                              errorStyle: const TextStyle(
-                                  color: Colors.redAccent, fontSize: 16.0),
-                              hintText: 'Please select a Food Type/s',
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(5.0))),
-                          isEmpty: _FoodTypes == '',
-                          child: DropdownButtonHideUnderline(
-                            child: DropdownButton<String>(
-                              value: _FoodTypes.last,// change to depend on the selected item + other items if needed
-                              isDense: true,
-                              onChanged: null, // add a functionality to onChanged
-                              items: _FoodTypes.map((String value) {
-                                return DropdownMenuItem<String>(
-                                  value: value,
-                                  child: Text(value),
-                                );
-                              }).toList(),
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                    Padding(padding: EdgeInsets.only(bottom: 10)),
-                    TextFormField(
-                      keyboardType: TextInputType.number,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'OpenSans',
+    return Column(
+      children:<Widget>[
+       Container(
+         height: 5.h,
+           child: Center(child: Name),
+       ), // replace after back end is connected for further customization
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 2.w),
+          child: SizedBox(
+           height:45.h,
+           child: Form(
+            key: _formKey,
+              child: Column(
+              children: <Widget>[
+                FormField(
+                  builder: (FormFieldState<String> state) {
+                    return InputDecorator(
+                      decoration: InputDecoration(
+                          errorStyle: const TextStyle(
+                              color: Colors.redAccent, fontSize: 16.0),
+                          hintText: 'Please select a Food Type/s',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0))),
+                      isEmpty: _FoodTypes == '',
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton<String>(
+                          value: _FoodTypes.last,// change to depend on the selected item + other items if needed
+                          isDense: true,
+                          onChanged: null, // add a functionality to onChanged
+                          items: _FoodTypes.map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
                       ),
-                      decoration: const InputDecoration(
-                        label: Text('Food Waste Weight'),
-                        border: const OutlineInputBorder(),
-                        hintText: 'Your Answer',
-                        contentPadding: EdgeInsets.all(10.0),
-                      ),
-                      onChanged: (value){
-                        Food_Waste_Weight = int.parse(value);
-                      },
-                      validator: (value){
-                        if (value == null || value.isEmpty){
-                          return 'Please Enter the Weight of the Food Waste';
-                        }
-                        return null;
-                      },
-                      onEditingComplete: () => FocusScope.of(context).nextFocus(),
-                    ),
-                    Padding(padding: EdgeInsets.only(bottom: 10)),
-                    TextFormField(
-                      keyboardType: TextInputType.number,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'OpenSans',
-                      ),
-                      decoration: const InputDecoration(
-                        label: Text('Water Weight'),
-                        border: OutlineInputBorder(),
-                        hintText: 'Your Answer',
-                        contentPadding: EdgeInsets.all(10.0),
-                      ),
-                      onChanged: (value){
-                        Water_Weight = int.parse(value);
-                      },
-                      validator: (value){
-                        if (value == null || value.isEmpty){
-                          return 'Please enter the Weight of the Water';
-                        }
-                        return null;
-                      },
-                      onEditingComplete: () => FocusScope.of(context).nextFocus(),
-                    ),
-                    Padding(padding: EdgeInsets.only(bottom: 10)),
-                    TextFormField(
-                      keyboardType: TextInputType.number,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'OpenSans',
-                      ),
-                      decoration: const InputDecoration(
-                        label: Text('Gallon Size Fertilizer You Drained'),
-                        border: OutlineInputBorder(),
-                        hintText: 'Your Answer',
-                        contentPadding: EdgeInsets.all(10.0),
-                      ),
-                      onChanged: (value){
-                        Gallon_Fertilizer = int.parse(value);
-                      },
-                      validator: (value){
-                        if (value == null || value.isEmpty){
-                          return 'Please enter the number of Gallon Size Fertilizer You Drained';
-                        }
-                        return null;
-                      },
-                      onEditingComplete: () => FocusScope.of(context).nextFocus(),
-                    ),
-                    Padding(padding: EdgeInsets.only(bottom: 10)),
-                    TextFormField(
-                      keyboardType: TextInputType.number,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'OpenSans',
-                      ),
-                      decoration: const InputDecoration(
-                        label: Text('16-Oz Size Fertilizer You Drained'),
-                        border: OutlineInputBorder(),
-                        hintText: 'Your Answer',
-                        contentPadding: EdgeInsets.all(10.0),
-                      ),
-                      onChanged: (value){
-                        Oz_Fertilizer = int.parse(value);
-                      },
-                      validator: (value){
-                        if (value == null || value.isEmpty){
-                          return 'Please enter the number of 16-Oz Size Fertilizer You Drained';
-                        }
-                        return null;
-                      },
-                      onEditingComplete: () => FocusScope.of(context).nextFocus(),
-                    ),
-                    Padding(padding: EdgeInsets.only(bottom: 10)),
-                    TextFormField(
-                      keyboardType: TextInputType.number,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontFamily: 'OpenSans',
-                      ),
-                      decoration: const InputDecoration(
-                        label: Text('Temperature (Fahrenheit)'),
-                        border: const OutlineInputBorder(),
-                        hintText: 'Your Answer',
-                        contentPadding: EdgeInsets.all(10.0),
-                      ),
-                      onChanged: (value){
-                        Temperature = int.parse(value);
-                      },
-                      validator: (value){
-                        if (value == null || value.isEmpty){
-                          return 'Please enter the Guts' 'Temperature';
-                        }
-                        return null;
-                      },
-                      onEditingComplete: () => FocusScope.of(context).nextFocus(),
-                    ),
-                  ],
+                    );
+                  },
                 ),
-                ),
-             ),
-           ),
-         ),
-          Padding(padding: EdgeInsets.only(bottom: 30)),
-          SizedBox(
-              height: 55,
-              width: 300,
-              //Button
-              child: ElevatedButton(
-                //Button Action
-                onPressed: (){if (_formKey.currentState!.validate()){
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('Adding Your Data to the Cloud FireStore'),
-                      )
-                  );
-                  SystemDataInputForm.add({'Name': Name.data,
-                    'Food_Type': _FoodTypes.last, //take the value of the text field
-                    'Food_Waste_Weight': Food_Waste_Weight,
-                    'Water_Weight': Water_Weight,
-                    'Gallon_Fertilizer': Gallon_Fertilizer,
-                    'Oz_Fertilizer': Oz_Fertilizer,
-                    'Temperature': Temperature,
-                    'Date':DateTime.now()})
-                      .then((value) => print('System Data Saved'))
-                      .catchError((error) => print('Something went wrong and was not able to add user;$error'));
-                }
-                },
-                //Button Text
-                child: const Text('Submit',
-                  style: TextStyle(
-                      fontSize: 26
+                Padding(padding: EdgeInsets.only(bottom: 10)),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'OpenSans',
                   ),
+                  decoration: const InputDecoration(
+                    label: Text('Food Waste Weight'),
+                    border: const OutlineInputBorder(),
+                    hintText: 'Your Answer',
+                    contentPadding: EdgeInsets.all(10.0),
+                  ),
+                  onChanged: (value){
+                    Food_Waste_Weight = int.parse(value);
+                  },
+                  validator: (value){
+                    if (value == null || value.isEmpty){
+                      return 'Please Enter the Weight of the Food Waste';
+                    }
+                    return null;
+                  },
+                  onEditingComplete: () => FocusScope.of(context).nextFocus(),
                 ),
-              )
-          ),
-      ],
-      ),
+                Padding(padding: EdgeInsets.only(bottom: 10)),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'OpenSans',
+                  ),
+                  decoration: const InputDecoration(
+                    label: Text('Water Weight'),
+                    border: OutlineInputBorder(),
+                    hintText: 'Your Answer',
+                    contentPadding: EdgeInsets.all(10.0),
+                  ),
+                  onChanged: (value){
+                    Water_Weight = int.parse(value);
+                  },
+                  validator: (value){
+                    if (value == null || value.isEmpty){
+                      return 'Please enter the Weight of the Water';
+                    }
+                    return null;
+                  },
+                  onEditingComplete: () => FocusScope.of(context).nextFocus(),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 10)),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'OpenSans',
+                  ),
+                  decoration: const InputDecoration(
+                    label: Text('Gallon Size Fertilizer You Drained'),
+                    border: OutlineInputBorder(),
+                    hintText: 'Your Answer',
+                    contentPadding: EdgeInsets.all(10.0),
+                  ),
+                  onChanged: (value){
+                    Gallon_Fertilizer = int.parse(value);
+                  },
+                  validator: (value){
+                    if (value == null || value.isEmpty){
+                      return 'Please enter the number of Gallon Size Fertilizer You Drained';
+                    }
+                    return null;
+                  },
+                  onEditingComplete: () => FocusScope.of(context).nextFocus(),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 10)),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'OpenSans',
+                  ),
+                  decoration: const InputDecoration(
+                    label: Text('16-Oz Size Fertilizer You Drained'),
+                    border: OutlineInputBorder(),
+                    hintText: 'Your Answer',
+                    contentPadding: EdgeInsets.all(10.0),
+                  ),
+                  onChanged: (value){
+                    Oz_Fertilizer = int.parse(value);
+                  },
+                  validator: (value){
+                    if (value == null || value.isEmpty){
+                      return 'Please enter the number of 16-Oz Size Fertilizer You Drained';
+                    }
+                    return null;
+                  },
+                  onEditingComplete: () => FocusScope.of(context).nextFocus(),
+                ),
+                Padding(padding: EdgeInsets.only(bottom: 10)),
+                TextFormField(
+                  keyboardType: TextInputType.number,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontFamily: 'OpenSans',
+                  ),
+                  decoration: const InputDecoration(
+                    label: Text('Temperature (Fahrenheit)'),
+                    border: const OutlineInputBorder(),
+                    hintText: 'Your Answer',
+                    contentPadding: EdgeInsets.all(10.0),
+                  ),
+                  onChanged: (value){
+                    Temperature = int.parse(value);
+                  },
+                  validator: (value){
+                    if (value == null || value.isEmpty){
+                      return 'Please enter the Guts' 'Temperature';
+                    }
+                    return null;
+                  },
+                  onEditingComplete: () => FocusScope.of(context).nextFocus(),
+                ),
+              ],
+            ),
+            ),
+       ),
+        ),
+        Padding(padding: EdgeInsets.only(top: 3.h, bottom: 3.h)),
+        SizedBox(
+            height: 55,
+            width: 300,
+            //Button
+            child: ElevatedButton(
+              //Button Action
+              onPressed: (){if (_formKey.currentState!.validate()){
+                ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Adding Your Data to the Cloud FireStore'),
+                    )
+                );
+                SystemDataInputForm.add({'Name': Name.data,
+                  'Food_Type': _FoodTypes.last, //take the value of the text field
+                  'Food_Waste_Weight': Food_Waste_Weight,
+                  'Water_Weight': Water_Weight,
+                  'Gallon_Fertilizer': Gallon_Fertilizer,
+                  'Oz_Fertilizer': Oz_Fertilizer,
+                  'Temperature': Temperature,
+                  'Date':DateTime.now()})
+                    .then((value) => print('System Data Saved'))
+                    .catchError((error) => print('Something went wrong and was not able to add user;$error'));
+              }
+              },
+              //Button Text
+              child: const Text('Submit',
+                style: TextStyle(
+                    fontSize: 26
+                ),
+              ),
+            )
+        ),
+    ],
     );
       }
   }
