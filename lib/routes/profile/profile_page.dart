ebc17c0e7c31d30efd2e632196e41efaf4d8c564
fenghun31 <fujiahao31@gmail.com>
2022-07-20 13:34:53 -0400

    reformated the reminders page

--- lib/routes/profile/profile_page.dart
+++ lib/routes/profile/profile_page.dart
@@ -1,10 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:ecotone_app/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_cards/flutter_custom_cards.dart';
 import 'package:sizer/sizer.dart';
 import 'package:ecotone_app/NavBar.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:intl/intl.dart';
 
 
 Future <void> main() async{
@@ -18,11 +18,13 @@ class ProfilePage extends StatelessWidget {
       .instance
       .collection("Reminder")
       .snapshots();
+
+
   @override
   Widget build(BuildContext context) {
     return Sizer(
       builder: (BuildContext context, Orientation orientation, DeviceType deviceType) {
-    return Scaffold(
+      return Scaffold(
             //Header
             appBar:
             AppBar(
@@ -41,52 +43,49 @@ class ProfilePage extends StatelessWidget {
             body:
             //Top Text Container
             Column(
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(top: 15,
-                    left: 10
+                  padding: EdgeInsets.only(top: 2.h,
+                    left: 3.w
                   ),
                   child: Container(
                     child: Align(
                       alignment: Alignment.centerLeft,
-                      child: Padding(padding: EdgeInsets.only(left: 5),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text('Hi Employee 1',
-                                style: TextStyle(
-                                  fontSize: 17,
-                                    color: Color(0xFF166390),
-                                )
-                            ),
-                            Text('Reminder',
-                                style: TextStyle(
-                                  fontSize: 30,
-                                    color: Color(0xFF166390),
-                                )
-                            ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text('Hi Employee 1',
+                              style: TextStyle(
+                                fontSize: 12.sp,
+                                  color: Color(0xFF166390),
+                              )
+                          ),
+                          Text('Reminder',
+                              style: TextStyle(
+                                fontSize: 20.sp,
+                                  color: Color(0xFF166390),
+                              )
+                          ),
 
-                            Text('You have some tasks to complete',
-                                style: TextStyle(
-                                  fontSize: 15,
-                                    color: Color(0xFF166390),
-                                )
-                            ),
-                          ],
-                        ),
+                          Text('You have some tasks to complete',
+                              style: TextStyle(
+                                fontSize: 12.sp,
+                                  color: Color(0xFF166390),
+                              )
+                          ),
+                        ],
                       ),
                     )
                   ),
                 ),
                 Container(
-                    //Top Text Format
-                    alignment: Alignment.center,
-                    margin: EdgeInsets.all(5),
+                    margin: EdgeInsets.only(top:13.h,
+                    left: 1.w,
+                      right: 2.w,
+                    ),
                     child: Column(
                       children: <Widget>[
-                        //Card Formatting
-                        Padding(padding: EdgeInsets.all(10)),
-                        Text('Today'),
                         Padding(padding: EdgeInsets.all(10)),
                         Container(
                           height: 100,
@@ -110,126 +109,76 @@ class ProfilePage extends StatelessWidget {
                                     itemCount: data.size,
                                     scrollDirection: Axis.horizontal,
                                     itemBuilder:(context,index){
-                                      return Container(
-                                          height: 100,
-                                          width: 191,
-                                          child:ScrollConfiguration(
-                                            behavior: MyBehavior(),
-                                            child: ListView(
-                                              children: <Widget>[
-                                                CustomCard(
-                                                  borderRadius: 15,
-                                                  borderColor: Colors.blue,
-                                                  width: 25,
-                                                  color: Color(0xffe6eef3),
-                                                  child: ListTile(
-                                                    title: Text('${data.docs[index]['Time_Of_Event']}'),
-                                                    subtitle: Text('${data.docs[index]['Name_Of_Event']}'),
-                                                    trailing:
-                                                    IconButton(
-                                                      alignment: Alignment.centerRight,
-                                                      icon: Icon(
-                                                        Icons.alarm_add_outlined,
-                                                        size: 17.0,
-                                                        color: Colors.brown[900],
-                                                      ),
-                                                      onPressed: () {},
-                                                    ),
-                                                  ),
-                                                )
-                                              ],
+                                      return CustomCard(
+                                        borderRadius: 15,
+                                        borderColor: Colors.blue,
+                                        width: 200,
+                                        color: Color(0xffe6eef3),
+                                        child: ListTile(
+                                          title: Text(
+                                                DateFormat.yMMMd().add_jm().format(data
+                                                    .docs[index]['Time_Of_Event']
+                                                    .toDate())
+                                          ),
+                                          subtitle: Text('${data
+                                              .docs[index]['Name_Of_Event']}'
+                                          ),
+                                          trailing: IconButton(
+                                            alignment: Alignment.centerRight,
+                                            icon: Icon(
+                                              Icons.alarm_add_outlined,
+                                              size: 17.0,
+                                              color: Colors.brown[900],
                                             ),
-                                          )
+                                            onPressed: () {},
+                                          ),
+                                        ),
                                       );
                                     }
                                 );
                               },
-
                           ),
                         ),
                         //Card Formatting
-                        Padding(padding: EdgeInsets.all(10)),
-                        Text('This Week'),
-                        Padding(padding: EdgeInsets.all(10)),
-                        Container(
-                          height: 100,
-                          width: 500,
-                          child:ListView.builder(
-                              shrinkWrap: true,
-                              itemCount: 5,
-                              scrollDirection: Axis.horizontal,
-                              itemBuilder:(context,index){
-                                return Container(
-                                    height: 100,
-                                    width: 191,
-                                    child:ScrollConfiguration(
-                                      behavior: MyBehavior(),
-                                      child: ListView(
-                                        children: <Widget>[
-                                          CustomCard(
-                                            borderRadius: 15,
-                                            borderColor: Colors.blue,
-                                            width: 25,
-                                            color: Color(0xffe6eef3),
-                                            child: ListTile(
-                                              title: Text('time of the event'),
-                                              subtitle: Text('name of the event'),
-                                              trailing:
-                                              IconButton(
-                                                alignment: Alignment.centerRight,
-                                                icon: Icon(
-                                                  Icons.alarm_add_outlined,
-                                                  size: 17.0,
-                                                  color: Colors.brown[900],
-                                                ),
-                                                onPressed: () {},
-                                              ),
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                    )
-                                );
-                              }
-                          ),
-                        ),
-
-                        //Button Formatting
-                        //Button
-                        ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.blue,
-                              onPrimary: Colors.white,
-                              elevation: 4,
-                            ),
-                            //Button Action
-                            onPressed: () {},
-                            //Button Text
-                            child: const Text("InvolveMINT")
-                        ),
-
-                        //Button Formatting
-                        const Padding(padding: EdgeInsets.all(5)),
-                        //Button
-                        ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.red,
-                              onPrimary: Colors.white,
-                              elevation: 4,
-                            ),
-                            //Button Action
-                            onPressed: () {},
-                            //Button Text
-                            child: const Text('Sign Out')
-                        ),
-
                       ],
                     )
+                ),
+                Padding(padding: EdgeInsets.only(top:13.h)),
+                SizedBox(
+                  height: 7.h,
+                  width: 45.w,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.blue,
+                        onPrimary: Colors.white,
+                        elevation: 4,
+                      ),
+                      //Button Action
+                      onPressed: () {},
+                      //Button Text
+                      child: const Text("InvolveMINT")
+                  ),
+                ),
+                Padding(padding: EdgeInsets.all(3.h)),
+                SizedBox(
+                  height: 7.h,
+                  width: 45.w,
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.red,
+                        onPrimary: Colors.white,
+                        elevation: 4,
+                      ),
+                      //Button Action
+                      onPressed: () {},
+                      //Button Text
+                      child: const Text('Sign Out')
+                  ),
                 ),
                 Align(
                   alignment: Alignment.bottomRight,
                     child: Container(
-                      padding: EdgeInsets.only(right: 10),
+                      padding: EdgeInsets.only(right: 5.w),
                       child: FloatingActionButton(
                         tooltip:"Add Reminders",
                           child: Icon(Icons.add,
