c891adfd4fc444559c5ff1bc45fed087d57a4965
fenghun31 <fujiahao31@gmail.com>
2022-08-08 16:06:36 -0400

    fixed map paged

--- lib/routes/map/Map_Page.dart
+++ lib/routes/map/Map_Page.dart
@@ -4,25 +4,7 @@ import 'package:location/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:ecotone_app/NavBar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-Future <void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(Map_Page());
-}
-
-class Map_Page extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Hide the debug banner
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Google Maps Demo',
-      home: Map(),
-    );
-  }
-}
 
 class Map extends StatefulWidget {
   @override
