d2d5f16ea2769628f8c2a1d6b892f43f779879eb
Snickledap <kevinchen536@gmail.com>
2022-08-08 16:56:23 -0400

    Started new NavBar with Navigator stack

--- lib/NavBar.dart
+++ lib/NavBar.dart
@@ -1,10 +1,74 @@
+import 'package:ecotone_app/routes/profile/profile_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:ecotone_app/routes/map/Map_Page.dart';
 
 List<String> routeNames = ['/map', '/data_analytics', '/checklist', "/info", "/profile"];
 
+enum TabItem {map, checklist, info, profile, analytics}
 
-class NavBar extends StatefulWidget {
+const Map<TabItem, String> tabName = {
+  TabItem.map: 'Map',
+  TabItem.checklist: 'Checklist',
+  TabItem.info: 'Info',
+  TabItem.profile: 'Profile',
+  TabItem.analytics: 'Analytics',
+};
+
+const Map<TabItem, IconData> tabIcon = {
+  TabItem.map: Icons.location_on,
+  TabItem.checklist: Icons.checklist,
+  TabItem.info: Icons.help_outline,
+  TabItem.profile: Icons.person_outline,
+  TabItem.analytics: Icons.analytics,
+};
+
+class TabNavigatorRoutes {
+  static const String map = '/map';
+  static const String checklist = '/checklist';
+  static const String info = '/info';
+  static const String profile = '/profile';
+  static const String analytics = '/analytics';
+}
+
+class NavBar2 extends StatelessWidget {
+  NavBar2({required this.navigatorKey, required this.tabItem});
+  final GlobalKey<NavigatorState>? navigatorKey;
+  final TabItem tabItem;
+
+  void _push(BuildContext context) {
+    var routeBuilders = _routeBuilders(context);
+
+    Navigator.push(context,
+        MaterialPageRoute(
+            builder: (context) =>
+                routeBuilders[TabNavigatorRoutes.profile]!(context)),
+    );
+
+  }
+
+  Map<String, WidgetBuilder> _routeBuilders(BuildContext context) {
+    return {
+      TabNavigatorRoutes.profile: (context) => ProfilePage(),
+
+    };
+  }
+
+  Widget build(BuildContext context) {
+    final routeBuilders = _routeBuilders(context);
+
+    return Navigator(
+      key: navigatorKey,
+
+    );
+  }
+}
+
+
+
+
+
+//**************************************************************
+  class NavBar extends StatefulWidget {
   const NavBar({Key? key}) : super(key: key);
 
 
