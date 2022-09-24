cb877c0d55a86c491eb35d4397569a7043b92d25
Niles44 <chrissy3piece@gmail.com>
2022-09-24 19:37:55 -0400

    Consumer profile button

--- lib/routes/profile/Consumer_Profile.dart
+++ lib/routes/profile/Consumer_Profile.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ecotone_app/NavBar_Consumer.dart';
+import 'package:get/get.dart';
+
+import '../../main.dart';
+
 
 
 
 String points = "100";
-String username = "Chris Niles";
 
 
 
@@ -55,7 +58,7 @@ class _ConsumerProfileState extends State<_ConsumerProfile> {
 
               //Username Text
               Text(
-              "Hello, $username",
+              "Hello, Consumer",
               style: TextStyle(fontSize: 24),
               ),
 
@@ -115,11 +118,11 @@ class _ConsumerProfileState extends State<_ConsumerProfile> {
                     ),
 
                   //Shop Button Action
-                  onPressed: (){},
+                  onPressed: (){Get.to(() => RouteGenerator());},
 
                     //Shop Button Text
                     child: Text(
-                      "Shop",
+                      "Team Facing App",
                       style: TextStyle(fontSize: 24.0),
                       textAlign: TextAlign.center
                     ),
@@ -142,6 +145,7 @@ class _ConsumerProfileState extends State<_ConsumerProfile> {
 
                     //Sign Out Button Action
                     onPressed: (){},
+
 
                       //Sign Out Button Text
                       child: Text(
